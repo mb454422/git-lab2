d90f75634c2ece25f0ea5a4ac72062db4aa00263
mb454422 <mb454422@ohio.edu>
2023-09-10 19:47:01 -0400

    code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Matthew Bocuehr>
+ *        Date: <9-10-23>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+        cout << "hello git" << endl;
+    return 0;
+}// main
